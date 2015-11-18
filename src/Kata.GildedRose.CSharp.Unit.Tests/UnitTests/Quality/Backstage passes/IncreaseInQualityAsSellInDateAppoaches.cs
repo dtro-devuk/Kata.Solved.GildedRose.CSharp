f8e3f5f44783f3d6dd3a2f1202faff6f7f5a2b62
TheTalkingDev <TheTalkingDev@users.noreply.github.com>
2015-11-18 23:13:52 +0000

    Unit Tests for Backstage Passes - Quality Increases - PASS
    Quality increases the nearer we get to sell by date,
    by 1, 2, then 3.

---
+++ src/Kata.GildedRose.CSharp.Unit.Tests/UnitTests/Quality/Backstage passes/IncreaseInQualityAsSellInDateAppoaches.cs
@@ -0,0 +1,36 @@
+﻿using Kata.GildedRose.CSharp.Common.Testing.Builders;
+using Kata.GildedRose.CSharp.Console;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Kata.GildedRose.CSharp.Unit.Tests.UnitTests.Quality.Backstage_passes
+{
+    [TestFixture]
+    public class IncreaseInQualityAsSellInDateAppoaches : WhenTestingTheQuality
+    {
+        protected override void Setup()
+        {
+            StockItemUnderTest = ItemBuilder
+                .Build
+                .WithName("Backstage passes to a TAFKAL80ETC concert")
+                .WithQuality(ActualQualityValue)
+                .WithSellin(ActualSellinValue)
+                .AnInstance();
+
+            StockItemsUnderTest = new List<Item> { StockItemUnderTest };
+            GildedRoseConsole.Items = StockItemsUnderTest;
+        }
+
+        [TestCase(10, 20, 11)]
+        [TestCase(10, 10, 12)]
+        [TestCase(10, 5, 13)]
+        public void ItShouldReturnAHigherQualityAsSellinDateLowers(int actualQuality, int actualSellin, int expectedQuality)
+        {
+            ActualQualityValue = actualQuality;
+            ActualSellinValue = actualSellin;
+
+            ArrangeAndAct();
+            Assert.AreEqual(expectedQuality, GetFirstItemInInventory().Quality);
+        }
+    }
+}
