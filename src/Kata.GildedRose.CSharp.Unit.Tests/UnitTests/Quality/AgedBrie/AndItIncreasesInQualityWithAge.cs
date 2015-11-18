ca75b3961ed7e5443e24878d7f303a89108172e1
TheTalkingDev <TheTalkingDev@users.noreply.github.com>
2015-11-18 22:18:57 +0000

    Unit Tests for Aged Brie - PASS
    Unit tests for Aged Brie increases with Age Each Day as it ages

---
+++ src/Kata.GildedRose.CSharp.Unit.Tests/UnitTests/Quality/AgedBrie/AndItIncreasesInQualityWithAge.cs
@@ -0,0 +1,48 @@
+﻿using Kata.GildedRose.CSharp.Common.Testing.Builders;
+using Kata.GildedRose.CSharp.Console;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Kata.GildedRose.CSharp.Unit.Tests.UnitTests.Quality.AgedBrie
+{
+    [TestFixture]
+    public class AndItIncreasesInQualityWithAge : WhenTestingTheQuality
+    {
+        protected override void Setup()
+        {
+            ExpectedQualityValue = 11;
+            ActualQualityValue = 10;
+            ActualSellinValue = 10;
+            ActualName = "Aged Brie";
+
+            StockItemUnderTest = ItemBuilder
+                .Build
+                .WithName(ActualName)
+                .WithQuality(ActualQualityValue)
+                .WithSellin(ActualSellinValue)
+                .AnInstance();
+
+            StockItemsUnderTest = new List<Item> { StockItemUnderTest };
+
+            GildedRoseConsole.Items = StockItemsUnderTest;
+        }
+
+        [Test]
+        public void ItShouldReturnAnIncreaseInQuality()
+        {
+            ArrangeAndAct();
+            Assert.AreEqual(ExpectedQualityValue, GetFirstItemInInventory().Quality);
+        }
+
+        [Test]
+        public void ItShouldIncreaseByAFactorOfOne()
+        {
+            ArrangeAndAct();
+
+            var factor = ActualQualityValue - GetFirstItemInInventory().Quality;
+
+            Assert.AreEqual(1, Math.Abs(factor));
+        }
+    }
+}
