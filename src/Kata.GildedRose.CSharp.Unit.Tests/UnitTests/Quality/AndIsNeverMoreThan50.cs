ec8e465995b2bd423a288110af97d89ddff7f654
TheTalkingDev <TheTalkingDev@users.noreply.github.com>
2015-11-18 22:26:11 +0000

    Unit Tests - Quality Is Never More than 50
    Tests all items other than "Conjured" items at the moment

---
+++ src/Kata.GildedRose.CSharp.Unit.Tests/UnitTests/Quality/AndIsNeverMoreThan50.cs
@@ -0,0 +1,39 @@
+﻿using Kata.GildedRose.CSharp.Common.Testing.Builders;
+using Kata.GildedRose.CSharp.Console;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Kata.GildedRose.CSharp.Unit.Tests.UnitTests.Quality
+{
+    [TestFixture]
+    public class AndIsNeverMoreThan50 : WhenTestingTheQuality
+    {
+        protected override void Setup()
+        {
+            StockItemUnderTest = ItemBuilder
+                .Build
+                .WithName(ActualName)
+                .WithQuality(ActualQualityValue)
+                .WithSellin(ActualSellinValue)
+                .AnInstance();
+
+            StockItemsUnderTest = new List<Item> { StockItemUnderTest };
+            GildedRoseConsole.Items = StockItemsUnderTest;
+        }
+
+        [TestCase("+5 Dexterity Vest", 50, 0, 50)]
+        [TestCase("Aged Brie", 50, 0, 50)]
+        [TestCase("Sulfuras, Hand of Ragnaros", 50, 10, 50)]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 50, 0, 50)]
+        public void ItShouldNeverBeGreaterThanTheExpectedValue(string actualName, int actualQuality, int actualSellin, int expectedQuality)
+        {
+            ActualName = actualName;
+            ActualQualityValue = actualQuality;
+            ActualSellinValue = actualSellin;
+
+            ArrangeAndAct();
+
+            Assert.LessOrEqual(GetFirstItemInInventory().Quality, expectedQuality);
+        }
+}
+}
