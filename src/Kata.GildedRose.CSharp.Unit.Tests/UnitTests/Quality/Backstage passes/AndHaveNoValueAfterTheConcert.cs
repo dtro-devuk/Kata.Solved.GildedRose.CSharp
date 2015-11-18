cee1ff81855df2be813d3ad24980d1db306ae046
TheTalkingDev <TheTalkingDev@users.noreply.github.com>
2015-11-18 23:18:37 +0000

    Unit Tests - Backstage passes - No decrease - PASS
    No Decrease in quality after sell-by date has passed

---
+++ src/Kata.GildedRose.CSharp.Unit.Tests/UnitTests/Quality/Backstage passes/AndHaveNoValueAfterTheConcert.cs
@@ -0,0 +1,39 @@
+﻿using Kata.GildedRose.CSharp.Common.Testing.Builders;
+using Kata.GildedRose.CSharp.Console;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Kata.GildedRose.CSharp.Unit.Tests.UnitTests.Quality.Backstage_passes
+{
+    [TestFixture]
+    public class AndHaveNoValueAfterTheConcert : WhenTestingTheQuality
+    {
+        protected override void Setup()
+        {
+            //Sell by date 0 = sellby date passed
+
+            ActualQualityValue = 10;
+            ActualSellinValue = 0;
+            ExpectedQualityValue = 0;
+
+            StockItemUnderTest = ItemBuilder
+                .Build
+                .WithName("Backstage passes to a TAFKAL80ETC concert")
+                .WithQuality(ActualQualityValue)
+                .WithSellin(ActualSellinValue)
+                .AnInstance();
+
+            StockItemsUnderTest = new List<Item> { StockItemUnderTest };
+            GildedRoseConsole.Items = StockItemsUnderTest;
+        }
+
+        [Test]
+        public void ItShouldReturnNoValue()
+        {
+            ArrangeAndAct();
+
+            Assert.AreEqual(ExpectedQualityValue, GetFirstItemInInventory().Quality);
+        }
+
+    }
+}
