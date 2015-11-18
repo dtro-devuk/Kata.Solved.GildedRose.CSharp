a2adc19097e5e052448c8671a9f6e46dd99273eb
TheTalkingDev <TheTalkingDev@users.noreply.github.com>
2015-11-18 15:48:23 +0000

    Iteration 0.3 Add Unit Tests Project
    Add project and Nuget packages for NUnit and RhinoMocks

---
+++ src/Kata.GildedRose.CSharp.Unit.Tests/WhenTestTheGildedRoseProgram.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+namespace Kata.GildedRose.CSharp.Unit.Tests
+{
+    [TestFixture]
+    public class WhenTestTheGildedRoseProgram
+    {
+        [Test]
+        public void ItShouldDoSomething()
+        {
+            //Test case goes here
+        }
+    }
+}
