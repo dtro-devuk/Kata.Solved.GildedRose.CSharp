1713d0ebe1b00915d6d25a77f8b6a27d99b7fc2c
TheTalkingDev <TheTalkingDev@users.noreply.github.com>
2015-11-19 19:53:09 +0000

    TDD - FACTORY - Legends - FAIL - RED
    Not Implemented Exception

--- src/Kata.GildedRose.CSharp.Unit.Tests/Factories/UpdateStockItemStrategy/AndItemIsALegend.cs
+++ src/Kata.GildedRose.CSharp.Unit.Tests/Factories/UpdateStockItemStrategy/AndItemIsALegend.cs
@@ -4,19 +4,19 @@ using NUnit.Framework;
 namespace Kata.GildedRose.CSharp.Unit.Tests.Factories.UpdateStockItemStrategy
 {
     [TestFixture]
-    public class AndItemIsABackStagePass : WhenTestingTheFactory
+    public class AndItemIsALegend : WhenTestingTheFactory
     {
         protected override void Setup()
         {
-            ActualName = "Backstage passes to a TAFKAL80ETC concert";
+            ActualName = "Sulfuras, Hand of Ragnaros";
             base.Setup();
         }
 
         [Test]
-        public void Factory_BackStagePass_ItShouldReturnTheCorrectStrategy()
+        public void Factory_Legend_ItShouldReturnTheCorrectStrategy()
         {
             ArrangeAndAct();
-            Assert.IsInstanceOf<BackStagePassesUpdateStrategy>(Result);
+            Assert.IsInstanceOf<LegendaryItemsUpdateStratgey>(Result);
         }
     }
 }
