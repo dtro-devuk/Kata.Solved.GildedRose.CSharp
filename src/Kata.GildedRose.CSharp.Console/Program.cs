f77e13010c65874b3bdc31eec54a59906543774f
TheTalkingDev <TheTalkingDev@users.noreply.github.com>
2015-11-19 11:57:34 +0000

    Refactor Program - Backstage passes to a new else if statement - PASS
    Move "Back stage passes" out of previous if statement, and create a new
    else if, copy if (item quality < 50) statement into new condition from
    above

--- src/Kata.GildedRose.CSharp.Console/Program.cs
+++ src/Kata.GildedRose.CSharp.Console/Program.cs
@@ -40,29 +40,31 @@ namespace Kata.GildedRose.CSharp.Console
             {
                 if (item.Name == "Sulfuras, Hand of Ragnaros") { continue; }
 
-                if (item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Name == "Aged Brie")
                 {
                     if (item.Quality < 50)
                     {
                         item.Quality = item.Quality + 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    }
+                }
+                else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
+                    if (item.SellIn < 11)
+                    {
+                        if (item.Quality < 50)
                         {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
+                            item.Quality = item.Quality + 1;
+                        }
+                    }
+                    if (item.SellIn < 6)
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
                         }
                     }
                 }
