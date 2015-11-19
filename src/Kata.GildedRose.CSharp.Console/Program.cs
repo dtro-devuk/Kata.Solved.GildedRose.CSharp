d7c468a488895f58887cf3a9e042ba5a62c3d02e
TheTalkingDev <TheTalkingDev@users.noreply.github.com>
2015-11-19 20:52:34 +0000

    Refactor - Program - List Items from a new Method call
    SOC/SRP - cleaner code

--- src/Kata.GildedRose.CSharp.Console/Program.cs
+++ src/Kata.GildedRose.CSharp.Console/Program.cs
@@ -6,7 +6,7 @@ namespace Kata.GildedRose.CSharp.Console
 {
     public class Program
     {
-        public IList<Item> Items;
+        public IList<Item> Items; //You can't touch this
 
         IUpdateItemStrategyFactory _updateFactory;
 
@@ -21,25 +21,30 @@ namespace Kata.GildedRose.CSharp.Console
 
             var app = new Program()
             {
-                Items = new List<Item>
-                    {
-                        new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                        new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                        new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                        new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                        new Item
-                            {
-                                Name = "Backstage passes to a TAFKAL80ETC concert",
-                                SellIn = 15,
-                                Quality = 20
-                            },
-                        new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                    }
+                Items = GetDefaultInventory()
             };
 
             app.UpdateQuality();
 
             System.Console.ReadKey();
+        }
+
+        public static List<Item> GetDefaultInventory()
+        {
+            return new List<Item>
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Item
+                        {
+                            Name = "Backstage passes to a TAFKAL80ETC concert",
+                            SellIn = 15,
+                            Quality = 20
+                        },
+                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                };
         }
 
         public void UpdateQuality()
