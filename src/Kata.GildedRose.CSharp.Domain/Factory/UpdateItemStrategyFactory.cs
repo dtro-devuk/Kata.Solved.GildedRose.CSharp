4b638d677a1898c24d20b35c007460fb8ede47a8
TheTalkingDev <TheTalkingDev@users.noreply.github.com>
2015-11-19 19:49:47 +0000

    TDD - FACTORY TEST - Backstage passes - FAIL -  RED
    Implementing switch statement

--- src/Kata.GildedRose.CSharp.Domain/Factory/UpdateItemStrategyFactory.cs
+++ src/Kata.GildedRose.CSharp.Domain/Factory/UpdateItemStrategyFactory.cs
@@ -11,8 +11,7 @@ namespace Kata.GildedRose.CSharp.Domain.Factory
                 case "Aged Brie":
                     return new AgedBrieUpdateStrategy();
                 case "Backstage passes to a TAFKAL80ETC concert":
-                    throw new NotImplementedException();
-                //    return new BackStagePassesUpdateStrategy();
+                    return new BackStagePassesUpdateStrategy();
                 case "Sulfuras, Hand of Ragnaros":
                     throw new NotImplementedException();
                 //    return new LegendaryItemsUpdateStratgey();
---
+++ src/Kata.GildedRose.CSharp.Unit.Tests/Factories/UpdateStockItemStrategy/AndItemIsABackStagePass.cs
@@ -0,0 +1,22 @@
+﻿using Kata.GildedRose.CSharp.Domain;
+using NUnit.Framework;
+
+namespace Kata.GildedRose.CSharp.Unit.Tests.Factories.UpdateStockItemStrategy
+{
+    [TestFixture]
+    public class AndItemIsABackStagePass : WhenTestingTheFactory
+    {
+        protected override void Setup()
+        {
+            ActualName = "Backstage passes to a TAFKAL80ETC concert";
+            base.Setup();
+        }
+
+        [Test]
+        public void Factory_AgedBrie_ItShouldReturnTheCorrectStrategy()
+        {
+            ArrangeAndAct();
+            Assert.IsInstanceOf<BackStagePassesUpdateStrategy>(Result);
+        }
+    }
+}
