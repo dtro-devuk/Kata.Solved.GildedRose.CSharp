5f0e1bdf77261b84238a8ca84a79052a213c97d9
TheTalkingDev <TheTalkingDev@users.noreply.github.com>
2015-11-19 18:51:12 +0000

    Test we can commit gain

--- src/Kata.GildedRose.CSharp.Unit.Tests/Factories/UpdateStockItemStrategy/UpdateItemStrategyFactory.cs
+++ src/Kata.GildedRose.CSharp.Unit.Tests/Factories/UpdateStockItemStrategy/UpdateItemStrategyFactory.cs
@@ -6,7 +6,21 @@ namespace Kata.GildedRose.CSharp.Unit.Tests.Factories.UpdateStockItemStrategy
     {
         public IStockItemUpdateStrategy Create(Item stockItem)
         {
-            return new AgedBrieUpdateStrategy();
+            switch (item.Name)
+            {
+                case "Aged Brie":
+                    _updateStrategy = new AgedBrieUpdateStrategy();
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    _updateStrategy = new BackStagePassesUpdateStrategy();
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    _updateStrategy = new LegendaryItemsUpdateStratgey();
+                    break;
+                default:
+                    _updateStrategy = new StandardItemsUpdateStrategy();
+                    break;
+            }
         }
     }
 }
